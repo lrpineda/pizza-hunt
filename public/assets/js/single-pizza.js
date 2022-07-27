c12f3468b507dc916ca7ac53a1f8ea644dbedccc
lrpineda <luicks212@gmail.com>
2022-07-26 23:06:49 -0600

    adding reply and UI changes

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -9,6 +9,28 @@ const $newCommentForm = document.querySelector('#new-comment-form');
 
 let pizzaId;
 
+function getPizza() {
+  // get id of pizza
+  const searchParams = new URLSearchParams(document.location.search.substring(1));
+  const pizzaId = searchParams.get('id');
+
+  // get pizza from api
+  fetch(`/api/pizzas/${pizzaId}`)
+    .then(res => {
+      if(!res.ok) {
+        throw new Error({ message: 'Something went wrong' });
+      }
+      return res.json();
+    })
+    .then(printPizza)
+    .catch(err => {
+      console.log(err);
+      alert('Cannot find a pizza with this id! Taking you back.')
+      window.history.back();
+    });
+
+}
+
 function printPizza(pizzaData) {
   console.log(pizzaData);
 
@@ -87,6 +109,28 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
+
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(res => {
+      if(!res.ok) {
+        throw new Error({ message: 'Something went wrong' });
+      }
+      return res.json();
+    })
+    .then(commentRes => {
+      console.log(commentRes);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err)
+    });
 }
 
 function handleNewReplySubmit(event) {
@@ -106,6 +150,28 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(res => {
+      if(!res.ok) {
+        throw new Error({ message: 'Something went wrong' });
+      }
+      return res.json();
+    })
+    .then(commentRes => {
+      console.log(commentRes);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err)
+    });
 }
 
 $backBtn.addEventListener('click', function() {
@@ -114,3 +180,5 @@ $backBtn.addEventListener('click', function() {
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+
+getPizza();
